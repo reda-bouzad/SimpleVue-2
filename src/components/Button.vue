4d3d4216e4a16e1e2195f215d144bf2b08f67fe6
reda-bouzad <redabouzadfst@gmail.com>
2023-07-24 17:03:12 +0100

    'v1'

---
+++ src/components/Button.vue
@@ -0,0 +1,26 @@
+<template>
+  <button @click="onClick()" v-bind:style="{ background: color }" class="btn">
+    {{ title }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    title: {
+      type: String,
+      default: "click me!",
+    },
+    color: {
+      type: String,
+    },
+  },
+  methods: {
+    onClick() {
+      console.log("click");
+    },
+  },
+};
+</script>
+
---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button color="#4682a9" title="add task" />
+  </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+
+export default {
+  name: "Header",
+  props: {
+    title: {
+      type: String,
+      default: "Title",
+    },
+  },
+  components: {
+    Button,
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
